208ddfe493b3da4af3c4ad704e90ce6dd52e379a
reddy111111 <vnvreddy9938@gmail.com>
2023-08-21 20:03:28 +0530

    code changes and time functionality

--- sr.py
+++ sr.py
@@ -1,42 +1,53 @@
-import keyboard
-import pyttsx3
-import pygetwindow as gw
+import keyboard, pyttsx3, datetime
+import pygetwindow as pw
 
-# Initialize the speech synthesizer
-
+engine = pyttsx3.init()
 
 def speak_text(text):
-    engine = pyttsx3.init()
     """Function to convert text to speech"""
     engine.say(text)
-    engine.runAndWait()  # Process the speech request asynchronously
-    engine.stop()  # Stop the speech synthesizer
+    engine.runAndWait()
 
-def process_key_event(e):
-    """Process keyboard events and speak the pressed key"""
-    if e.event_type == keyboard.KEY_DOWN:
-        key_name = e.name
-        if key_name:
-            print(f"Key Pressed: {key_name}")
-            speak_text(key_name)
+def exit_program():
+    speak_text("exitting AudibleView")
+    exit()
 
-# Hook to capture keyboard events
-keyboard.hook(process_key_event)
+def get_window():
+    active_window=pw.getActiveWindow()
+    if(active_window):
+        speak_text("title is %s." % active_window.title)
+    else:
+        speak_text("unable to get title")
 
-# Run the screen reader
-try:
-    print("Screen Reader is running. Press 'Ctrl + C' to exit.")
-    while True:
-#        engine.iterate()  # Process the speech requests
+def current_time():
+    curren_time = datetime.datetime.now().time()
+    curren_time = curren_time.strftime("%H:%M")# properly formatting the time
+    speak_text(curren_time)
 
-        # Check for "Insert + T" key combination
-        if keyboard.is_pressed("insert+t"):
-            active_window = gw.getActiveWindow()
-            if active_window:
-                window_title = active_window.title
-                print(f"Active Window Title: {window_title}")
-                speak_text(f"Active Window Title: {window_title}")
+def current_date():
+    curren_date = datetime.datetime.now().date()
+    curren_date = curren_date.strftime("%d-%m-%Y")# properly formatting the date
+    speak_text(curren_date)
 
-except KeyboardInterrupt:
-    print("Screen Reader is exiting.")
+# Define the key combinations and their associated functions
+key_combinations = {
+    "alt+d": exit_program,
+    "insert+t": get_window,
+    "f12+insert": current_time,
+    "f12+insert,f12": current_date,
+    # Add more combinations as needed
+}
 
+def main():
+    print("AudibleView is running")
+    speak_text("starting AudibleView")
+    while True:  # main loop
+        hotkey = keyboard.read_hotkey(suppress=False)
+        if hotkey in key_combinations:
+            key_combinations[hotkey]()
+        elif hotkey:
+            print("key pressed: %s \n" % hotkey)
+            speak_text("%s" % hotkey)
+
+if __name__ == "__main__":  # don't run if imported
+    main()
